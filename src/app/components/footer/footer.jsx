e4702af7605f61243d4c5ecf2f2e499a9770fa6f
KrvMarina <142495792+KrvMarina@users.noreply.github.com>
2024-04-17 16:21:09 +0300

    style for footer

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import styles from './page.style.css'
+import Link from "next/link";
+
+import styles from './page.module.css'
 
 const Footer = () => {
     return (
         <div className={styles.container}>
             <div>©2023 OpenMind. All rights reserved.</div>
+            <form action="">
+                <h3>Form</h3>
+                <input type="text" placeholder="Enter name" />
+            </form>
         </div>
+
     )
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -14,13 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body >
-        <div className="container">
+        <div className="container center-container">
           <Navbar />
           {children}
-          <Footer />
+        </div>
+        <div className="footer-back">
+          <div className="container">
+            <Footer />
+          </div>
         </div>
       </body>
-
     </html>
   );
 }
