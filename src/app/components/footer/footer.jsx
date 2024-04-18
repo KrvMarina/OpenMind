479f676a200f4635e91bcc650695a14348c27609
KrvMarina <142495792+KrvMarina@users.noreply.github.com>
2024-04-18 14:16:35 +0300

    add root

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
-
 import styles from './page.module.css'
+
 
 const Footer = () => {
     return (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,15 @@
-//import Image from "next/image";
-//import styles from "./page.module.css";
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
 //import Hero from "public/hero.png";
 //import Button from "@/components/Button/Button";
 
 export default function Home() {
     return (
         <div >
-            main
+            <h1 className={styles.titel}>Open your mind to new possibilities</h1>
+            <p className={styles.subtitle}>Share your thoughts and knowledge with the world</p>
+
         </div>
     );
 }
