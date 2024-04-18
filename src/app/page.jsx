894dd0119be8018dd27b4a21b128a7f8a12caedb
KrvMarina <142495792+KrvMarina@users.noreply.github.com>
2024-04-18 18:19:04 +0300

    add button component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,19 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import global from "./globals.css";
+import Button from "./components/button/button";
 //import Hero from "public/hero.png";
 //import Button from "@/components/Button/Button";
 
 export default function Home() {
     return (
-        <div >
-            <h1 className={styles.titel}>Open your mind to new possibilities</h1>
-            <p className={styles.subtitle}>Share your thoughts and knowledge with the world</p>
-
+        <div className={styles.container}>
+            <div className={styles.text_block}>
+                <h1 className={styles.titel}>Open your mind to new possibilities</h1>
+                <p className={styles.subtitle}>Share your thoughts and knowledge with the world</p>
+                <Button url="/about" text="Start"></Button>
+            </div>
         </div>
     );
 }
