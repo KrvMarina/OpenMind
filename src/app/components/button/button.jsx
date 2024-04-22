0326bca1d924aab10b6d518ad9cbbb957ace7750
KrvMarina <142495792+KrvMarina@users.noreply.github.com>
2024-04-22 17:30:47 +0300

    ui contact

--- src/app/components/button/button.jsx
+++ src/app/components/button/button.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import styles from './button.module.css'
 import Link from "next/link";
 
-const Button = ({ text, url }) => {
+const Button = ({ text, url, width }) => {
     return (
         <Link href={url}>
-            <button className={styles.container}>{text}</button>
+            <button className={styles.container} style={{ width }}>{text}</button>
         </Link>
     )
 }
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-import styles from './page.style.css'
+import styles from './page.module.css'
+import Image from "next/image";
+import Button from "../components/button/button";
 
 const Contact = () => {
     return (
         <div className={styles.container}>
-            Contact
+            <h2 className={styles.titel}>Contact</h2>
+            <h3 className={styles.titel_h3}>Let's Keep in Touch</h3>
+            <div className={styles.content}>
+                <form className={styles.form}>
+                    <label htmlFor="name" className={styles.label}>Name</label>
+                    <input type="text" id="name" placeholder="Enter name" className={styles.input} />
+                    <label htmlFor="email" className={styles.label}>Email</label>
+                    <input type="text" id="email" placeholder="Enter email" className={styles.input} />
+                    <label htmlFor="message" className={styles.label}>Message</label>
+                    <textarea
+                        className={styles.text_area}
+                        id="message"
+                        placeholder="Enter message"
+                        rows="10"
+                    ></textarea>
+                    <Button url="#" text="Send" width="100%" />
+                </form>
+            </div>
         </div>
     )
 }
