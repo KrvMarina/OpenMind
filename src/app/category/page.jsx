4d825689436ae0123142d33f1a7584996a86e387
KrvMarina <142495792+KrvMarina@users.noreply.github.com>
2024-04-23 15:16:40 +0300

    ui Category

--- src/app/category/page.jsx
+++ src/app/category/page.jsx
@@ -11,40 +11,74 @@ import { BiWorld } from "react-icons/bi";
 import { FaPlus } from "react-icons/fa";
 
 
+const categories = [
+    {
+        href: "#",
+        icon: <FaHeart />,
+        title: "Life",
+        description: "Food, Home, Health, Family, Relationships, Pets"
+    },
+    {
+        href: "#",
+        icon: <FaPerson />,
+        title: "Self Improvement",
+        description: "Mental Health, Productivity, Mindfulness"
+    },
+    {
+        href: "#",
+        icon: <MdOutlineWork />,
+        title: "Work",
+        description: "Marketing, Business, Leadership, Remote Work"
+    },
+    {
+        href: "#",
+        icon: <GrTechnology />,
+        title: "Technology",
+        description: "Blockchain, Data Science, Gadgets, Security, Artificial Intelligence"
+    },
+    {
+        href: "#",
+        icon: <HiMiniComputerDesktop />,
+        title: "Software Development",
+        description: "Programming, Programming Languages, Dev Ops, Operating Systems"
+    },
+    {
+        href: "#",
+        icon: <FaPerson />,
+        title: "Media",
+        description: "Writing, Art, Gaming, Humor, Movies, Music, Photography, Television"
+    },
+    {
+        href: "#",
+        icon: <IoPeople />,
+        title: "Culture",
+        description: "Philosophy, Religion, Spirituality, Fashion, Beauty, Language, Sports"
+    },
+    {
+        href: "#",
+        icon: <BiWorld />,
+        title: "World",
+        description: "Cities, Nature, Travel"
+    }
+
+];
+
 const Category = () => {
     return (
         <div className={styles.container}>
-            <Link href="/topic">Topic</Link>
-            <div className={styles.card}>
-                <FaHeart /> <h3 className={styles.titel_h3}>Life</h3>
+            <h2 className={styles.titel}>Category</h2>
+            <div className={styles.cards_container}>
+                {categories.map((category, index) => (
+                    <Link key={index} href={category.href}>
+                        <div className={styles.card}>
+                            <h4 className={styles.title_card}>{category.icon}{category.title}</h4>
+                            <p>{category.description}</p>
+                        </div>
+                    </Link>
+                ))}
             </div>
-            <div className={styles.card}>
-                <FaPerson /> <h3 className={styles.titel_h3}>Self improvement</h3>
-            </div>
-            <div className={styles.card}>
-                <MdOutlineWork /> <h3 className={styles.titel_h3}>Wokr</h3>
-            </div>
-            <div className={styles.card}>
-                <GrTechnology /> <h3 className={styles.titel_h3}>Technology</h3>
-            </div>
-            <div className={styles.card}>
-                <HiMiniComputerDesktop /> <h3 className={styles.titel_h3}>Software Development</h3>
-            </div>
-            <div className={styles.card}>
-                <FaPerson /> <h3 className={styles.titel_h3}>Media</h3>
-            </div>
-            <div className={styles.card}>
-                <IoPeople /> <h3 className={styles.titel_h3}>Culture</h3>
-            </div>
-            <div className={styles.card}>
-                <BiWorld /> <h3 className={styles.titel_h3}>World</h3>
-            </div>
-            <div className={styles.card}>
-                <FaPlus /> <h3 className={styles.titel_h3}>Other</h3>
-            </div>
-
         </div>
-    )
-}
+    );
+};
 
 export default Category;
