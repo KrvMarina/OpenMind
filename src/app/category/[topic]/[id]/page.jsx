fb7491bd99d9b96d0a465cd5ccdd327b0c89a68d
KrvMarina <142495792+KrvMarina@users.noreply.github.com>
2024-04-24 23:03:12 +0300

    add file

---
+++ src/app/category/[topic]/[id]/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from './page.module.css'
+import Button from "../components/button/button";
+import Image from "next/image";
+
+const About = () => {
+    return (
+        <div className={styles.container}>
+
+            <h2 className={styles.titel}>About</h2>
+
+        </div>
+    )
+}
+
+export default About;
