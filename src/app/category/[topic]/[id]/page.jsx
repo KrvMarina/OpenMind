15dcf473f23982d8117a9858deb0eb5f63a52303
KrvMarina <142495792+KrvMarina@users.noreply.github.com>
2024-04-26 00:28:55 +0300

    start Post page

--- src/app/category/[topic]/[id]/page.jsx
+++ src/app/category/[topic]/[id]/page.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-import styles from './page.module.css'
-import Button from "../components/button/button";
+import styles from './page.module.css';
+import { RxAvatar } from "react-icons/rx";
 import Image from "next/image";
 
-const About = () => {
+
+const Post = ({ tags }) => {
     return (
         <div className={styles.container}>
+            <h2 className={styles.titel}>Always Hungry and Tired? Here Are 8 Potential Reasons Why</h2>
+            <p className={styles.author}>
+                <RxAvatar className={styles.avatar} />Anna
+            </p>
+            <Image
+                src="/photo.png"
+                width={500}
+                height={500}
+                alt="photo"
+                className={styles.img}>
+            </Image>
 
-            <h2 className={styles.titel}>About</h2>
+            <p>
+                While everyone has different needs, Mercer says that the Valencia diet generally aligns with current dietary guidelines (as long as you're eating adequate amounts).
+
+                There isn’t much research or scientific evidence specifically supporting the Valencia diet, but because it's so similar to the Mediterranean diet, Mercer says some of the studies around that can apply—especially when you consider the emphasis on plant-based eating.
+
+                “In terms of the reduction of meat consumption, we often see less burden on our kidneys, and lower cholesterol levels,” Mercer explains.
+
+                Not only that, the Valencia diet may be less restrictive than other fad diets. “Since the Valencia diet emphasizes more of a change in the types of food we eat rather than restricting food/calories, it is more balanced and sustainable than some other popular diets,” Mercer says. “It is always better to add in more variety to our foods than to restrict food groups.”
+
+                She adds that the diet may be especially good for anyone interested in eating more whole foods or transitioning to more of a plant-based way of eating, along with anyone concerned about their heart health.
+            </p>
 
         </div>
-    )
-}
+    );
+};
 
-export default About;
+
+export default Post;
--- src/app/category/[topic]/page.jsx
+++ src/app/category/[topic]/page.jsx
@@ -9,9 +9,9 @@ import { RxAvatar } from "react-icons/rx";
 const Topic = ({ params }) => {
     return (
         <div className={styles.container}>
-            <h1>{decodeURIComponent(params.topic)}</h1>
+            <h2 className={styles.titel}>{decodeURIComponent(params.topic)}</h2>
             <div className={styles.cards_container}>
-                <Link href="#" >
+                <Link href="./topic/1">
                     <div className={styles.card}>
                         <Image
                             src="/photo.png"
@@ -20,13 +20,53 @@ const Topic = ({ params }) => {
                             alt="photo"
                             className={styles.img}>
                         </Image>
-                        <p className={styles.small_text}>
+                        <p className={styles.author}>
                             <RxAvatar className={styles.avatar} />Anna
                         </p>
                         <h4 className={styles.title_card}>Always Hungry and Tired? Here Are 8 Potential Reasons Why</h4>
                         <div className={styles.icons}>
                             <p><IoEyeSharp />200</p>
                             <p><FaHeart />68</p>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link href="/topic/2" >
+                    <div className={styles.card}>
+                        <Image
+                            src="/photo.png"
+                            width={500}
+                            height={500}
+                            alt="photo"
+                            className={styles.img}>
+                        </Image>
+                        <p className={styles.author}>
+                            <RxAvatar className={styles.avatar} />Alan
+                        </p>
+                        <h4 className={styles.title_card}>10 Ways to Lose More Weight Walking</h4>
+                        <div className={styles.icons}>
+                            <p><IoEyeSharp />100</p>
+                            <p><FaHeart />28</p>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link href="/topic/3" >
+                    <div className={styles.card}>
+                        <Image
+                            src="/photo.png"
+                            width={500}
+                            height={500}
+                            alt="photo"
+                            className={styles.img}>
+                        </Image>
+                        <p className={styles.author}>
+                            <RxAvatar className={styles.avatar} />Nik
+                        </p>
+                        <h4 className={styles.title_card}>The Basics of Body Recomposition</h4>
+                        <div className={styles.icons}>
+                            <p><IoEyeSharp />500</p>
+                            <p><FaHeart />98</p>
                         </div>
                     </div>
                 </Link>
