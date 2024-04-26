8dfb1827cfec4a4e435fb8a0d24d1e803f0beed1
KrvMarina <142495792+KrvMarina@users.noreply.github.com>
2024-04-26 13:09:14 +0300

    ui Post page

--- src/app/category/[topic]/[id]/page.jsx
+++ src/app/category/[topic]/[id]/page.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import styles from './page.module.css';
 import { RxAvatar } from "react-icons/rx";
 import Image from "next/image";
+import { FaHeart } from "react-icons/fa";
 
 
-const Post = ({ tags }) => {
+const Post = () => {
     return (
         <div className={styles.container}>
+
             <h2 className={styles.titel}>Always Hungry and Tired? Here Are 8 Potential Reasons Why</h2>
-            <p className={styles.author}>
-                <RxAvatar className={styles.avatar} />Anna
-            </p>
+            <div className={styles.author}>
+                <p className={styles.avatar}>
+                    <RxAvatar className={styles.icon} />Anna
+                </p>
+                <p className={styles.heart}>
+                    <FaHeart className={styles.icon} />68
+                </p>
+            </div>
             <Image
                 src="/photo.png"
                 width={500}
@@ -19,18 +26,41 @@ const Post = ({ tags }) => {
                 className={styles.img}>
             </Image>
 
-            <p>
-                While everyone has different needs, Mercer says that the Valencia diet generally aligns with current dietary guidelines (as long as you're eating adequate amounts).
-
-                There isn’t much research or scientific evidence specifically supporting the Valencia diet, but because it's so similar to the Mediterranean diet, Mercer says some of the studies around that can apply—especially when you consider the emphasis on plant-based eating.
-
-                “In terms of the reduction of meat consumption, we often see less burden on our kidneys, and lower cholesterol levels,” Mercer explains.
-
-                Not only that, the Valencia diet may be less restrictive than other fad diets. “Since the Valencia diet emphasizes more of a change in the types of food we eat rather than restricting food/calories, it is more balanced and sustainable than some other popular diets,” Mercer says. “It is always better to add in more variety to our foods than to restrict food groups.”
-
-                She adds that the diet may be especially good for anyone interested in eating more whole foods or transitioning to more of a plant-based way of eating, along with anyone concerned about their heart health.
+            <p className={styles.post_text}>
+                While everyone has different needs, Mercer says that the Valencia diet generally
+                aligns with current dietary guidelines (as long as you're eating adequate amounts).
+                There isn't much research or scientific evidence specifically supporting the Valencia
+                diet, but because it's so similar to the Mediterranean diet, Mercer says some of the
+                studies around that can apply—especially when you consider the emphasis on plant-based eating.
+                “In terms of the reduction of meat consumption, we often see less burden on our kidneys,
+                and lower cholesterol levels,” Mercer explains.
+                Not only that, the Valencia diet may be less restrictive than other fad diets.
+                “Since the Valencia diet emphasizes more of a change in the types of food we eat rather
+                than restricting food/calories, it is more balanced and sustainable than some other popular
+                diets,” Mercer says. “It is always better to add in more variety to our foods than to
+                restrict food groups.”
+                She adds that the diet may be especially good for anyone interested in eating more whole
+                foods or transitioning to more of a plant-based way of eating, along with anyone concerned
+                about their heart health.
             </p>
 
+            <p className={styles.post_text}>
+                While everyone has different needs, Mercer says that the Valencia diet generally
+                aligns with current dietary guidelines (as long as you're eating adequate amounts).
+                There isn't much research or scientific evidence specifically supporting the Valencia
+                diet, but because it's so similar to the Mediterranean diet, Mercer says some of the
+                studies around that can apply—especially when you consider the emphasis on plant-based eating.
+                “In terms of the reduction of meat consumption, we often see less burden on our kidneys,
+                and lower cholesterol levels,” Mercer explains.
+                Not only that, the Valencia diet may be less restrictive than other fad diets.
+                “Since the Valencia diet emphasizes more of a change in the types of food we eat rather
+                than restricting food/calories, it is more balanced and sustainable than some other popular
+                diets,” Mercer says. “It is always better to add in more variety to our foods than to
+                restrict food groups.”
+                She adds that the diet may be especially good for anyone interested in eating more whole
+                foods or transitioning to more of a plant-based way of eating, along with anyone concerned
+                about their heart health.
+            </p>
         </div>
     );
 };
